ebc1854ffe24aaea6edc3fa56acd77763b06b5fc
ReyLuisAngel <reysae@gmail.com>
2024-10-21 23:37:59 -0600

    Act11

---
+++ p119_Organismo/Animales.java
@@ -0,0 +1,8 @@
+// Animales.java
+package p119_Organismo;
+
+public interface Animales {
+    public void multiCelular();
+
+    public void sangreCaliente();
+}
---
+++ p119_Organismo/Ave.java
@@ -0,0 +1,8 @@
+// Ave.java
+package p119_Organismo;
+
+public interface Ave extends Animales {
+    public void volar();
+
+    public void dosPatas();
+}
